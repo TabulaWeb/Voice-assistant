21a1caea53b5108a767d96bd82aef0af6e020053
Alex Tabula <67907195+TabulaWeb@users.noreply.github.com>
2021-08-01 16:40:40 +0300

    read

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
-const TelegramBot = require('node-telegram-bot-api');
-const axios = require('axios')
-const fs = require('fs')
+import TelegramBot from 'node-telegram-bot-api';
+import axios from 'axios';
+import fs from 'fs';
+import {TELEGRAM_API_KEY, YA_API_KEY} from './api.js';
 
-const TELEGRAM_API_KEY = '1913684884:AAF0FmR2EBx2Q-wHfnZFOELbhf3cZN98N8o';
-const YA_API_KEY = 'AQVNxZLk3itKM7UOw6rImer82gQvDaFeNMlxbkE8'
 
 const bot = new TelegramBot(TELEGRAM_API_KEY, {polling: true})
 
@@ -29,14 +28,8 @@ bot.on('voice', (msg) => {
         axios(axiosConfig)
             .then(response => {
                 const command = response.data.result
-                if(command == 'Выключи компьютер'){
-                    bot.sendMessage(msg.chat.id, 'Компьютер сейчас будет выключен')
-                }
-
-                if(command == 'Удали файл'){
-                    fs.unlinkSync('./text.txt')
-                    bot.sendMessage(msg.chat.id, 'Файл удален')
-                }
+                
+                bot.sendMessage(msg.chat.id, command)
             })
             .catch((err) => {
                 console.log('Ошибка распознования речи: ', err)
---
+++ readme.md
@@ -0,0 +1,16 @@
+# Голосовой помошник
+Данный голосовой помошник слушжит для автоматизации процессов, таких как выключение компьютера, запуск программ и все, что вашей душе угодно.
+
+При разработке использовались:
+- [node.js](https://nodejs.org/en/)
+- [node-telegram-bot-api](https://github.com/yagop/node-telegram-bot-api)
+- [axios](https://github.com/axios/axios)
+- [Yandex SpeechKit](https://console.cloud.yandex.ru/)
+
+## Установка 
+### Шаг 1
+Устанавливаем [node.js](https://nodejs.org/en/)
+
+### Шаг 2
+Скачиваем репозиторий и устанавливаем пакеты
+`yarn add`
